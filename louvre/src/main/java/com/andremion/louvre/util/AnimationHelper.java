e91860179465f5474a9f6ff9f62a504772d0016f
André Mion <andremion@gmail.com>
2020-11-03 14:26:38 +0000

    Update file headers

--- louvre/src/main/java/com/andremion/louvre/util/AnimationHelper.java
+++ louvre/src/main/java/com/andremion/louvre/util/AnimationHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017. André Mion
+ * Copyright (c) 2020. André Mion
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
